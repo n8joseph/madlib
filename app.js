59ca94c40303159e47b4f8cad3cfef6a05fe1652
n8joseph <natejoseph@Nates-MacBook-Pro.local>
2015-09-13 15:08:09 -0400

    added 2nd view

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ angular.module('myApp', [])
 	.controller('MyCtrl', function($scope) {
 		$scope.genders = ['male', 'female'];
 		$scope.mygender = { current: 'female' }
+		$scope.myview = 1
+		$scope.submit = function() {
+				
+		}
+		
 	});
